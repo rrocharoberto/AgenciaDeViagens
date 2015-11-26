dbbb29473091d35aa188e4e31868862c9696f148
Jonathan <jonathanrs1@hotmail.com>
2015-11-26 21:46:07 -0200

    Agencia/Hotel
    Iniciando integração

--- agencia/src/main/java/br/edu/univas/agencia/agencia/service/api/impl/HotelServiceImpl.java
+++ agencia/src/main/java/br/edu/univas/agencia/agencia/service/api/impl/HotelServiceImpl.java
@@ -7,10 +7,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import br.edu.univas.agencia.agencia.business.HotelBusiness;
 import br.edu.univas.agencia.agencia.service.api.HotelService;
 import br.edu.univas.agencia.exception.AgencyException;
 import br.edu.univas.agencia.model.Hotel;
 import br.edu.univas.agencia.model.HotelReserva;
+import br.edu.univas.agencia.model.Pacote;
 
 /**
  * Implementation of services defined in {@link HotelService}.
@@ -23,8 +25,10 @@ public class HotelServiceImpl implements HotelService {
 	@GET
 	@Override
 	public Collection<Hotel> listHotels(@QueryParam("packageId") int packageId) throws AgencyException {
-		// TODO return hotelBusiness.listHotels();
-		return null;
+		//PackageBusiness packageBusiness = new PackageBusiness();
+		//Pacote pacote = packageBusiness.retrievePackage(packageId);
+		HotelBusiness hotelBusiness = new HotelBusiness();
+		return hotelBusiness.listHotels(pacote);
 	}
 
 	@POST
