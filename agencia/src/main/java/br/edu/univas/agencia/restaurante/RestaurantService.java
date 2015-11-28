af60c5324e2fbe10879711630c665e5e14612aa6
Diego Nunes <diegodnunes12@gmail.com>
2015-11-28 09:33:17 -0200

    Service

--- agencia/src/main/java/br/edu/univas/agencia/restaurante/RestaurantService.java
+++ agencia/src/main/java/br/edu/univas/agencia/restaurante/RestaurantService.java
@@ -11,51 +11,124 @@ import java.util.List;
  * responsible by business logic to module of restaurant.
  */
 public class RestaurantService implements IRestaurant {
+    
+    RestaurantRepository repository;
 
     @Override
     public Restaurante createRestaurant(Restaurante restaurant) throws AgencyException {
-        //teste
+        
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
+        
         return null;
     }
 
     @Override
     public void updateRestaurant(Restaurante restaurant) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
     }
 
     @Override
     public void removeRestaurant(Integer restaurantId) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
     }
 
     @Override
     public Restaurante getRestauranteById(Integer restaurantId) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
         return null;
     }
 
     @Override
     public List<Restaurante> getRestaurantAll() throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
         return null;
     }
 
     @Override
     public List<Restaurante> getAvailableRestaurantList(Pacote bundle) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
         return null;
     }
 
     @Override
     public void createReserve(RestauranteReserva reserve) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
     }
 
     @Override
     public void removeReserve(Integer reserveId) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
     }
 
     @Override
     public RestauranteReserva getReserveById(Integer reserveId) throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
         return null;
     }
 
     @Override
     public List<RestauranteReserva> getReserveAll() throws AgencyException {
+        repository = new RestaurantRepository();
+        
+        try {
+            
+        } catch (Exception e) {
+            new AgencyException("Houve uma falha ao inserir o restaurante");
+        }
         return null;
     }
 
