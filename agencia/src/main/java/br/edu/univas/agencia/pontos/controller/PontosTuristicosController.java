d97012c9a583b72247c7cb7663284e051b4ff5bd
rafaelwsilva <rws1602@gmail.com>
2015-12-03 12:55:43 -0200

    Cadastro de pontos turisticos

---
+++ agencia/src/main/java/br/edu/univas/agencia/pontos/controller/PontosTuristicosController.java
@@ -0,0 +1,109 @@
+package br.edu.univas.agencia.pontos.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
+
+import util.HibernateUtil;
+import br.edu.univas.agencia.agencia.dao.CityDAO;
+import br.edu.univas.agencia.model.Cidade;
+import br.edu.univas.agencia.model.PontoTuristico;
+import br.edu.univas.agencia.pontos.pontosDAO.PontosTuristicosDAO;
+
+@ManagedBean(name = "pontosTuristicosController")
+@ViewScoped
+public class PontosTuristicosController {
+
+	private List<PontoTuristico> pontosTuristicos;
+	private List<Cidade> cidades;
+	private PontosTuristicosDAO ptDAO;
+	private CityDAO cityDao;
+	
+	private PontoTuristico pontoTuristico;
+	private PontoTuristico pontoTuristicoToEdit;
+	private PontoTuristico pontoTuristicoToDelete;
+	
+	@PostConstruct
+	public void init(){
+		ptDAO = new PontosTuristicosDAO(HibernateUtil.getEntityManager());
+		cityDao = new CityDAO(HibernateUtil.getEntityManager());
+		pontosTuristicos = ptDAO.listAllAttractions();
+		cidades = cityDao.listAllCities();
+		pontoTuristico = new PontoTuristico();
+	}
+	
+	public void addPontoTuristico(){
+		ptDAO.addPontoTuristico(pontoTuristico);
+		pontosTuristicos.add(pontoTuristico);
+		
+		RequestContext request = RequestContext.getCurrentInstance();
+		request.execute("PF('addPonto').hide()");
+		request.update("formAddPontos");
+		pontoTuristico = new PontoTuristico();
+		sendMessageToView("Ponto turístico adicionado com sucesso!", FacesMessage.SEVERITY_INFO);
+	}
+	
+	public void loadPontoTuristicoToEdit(PontoTuristico pontoTuristico){
+		pontoTuristicoToEdit = pontoTuristico;
+	}
+	
+	public void loadPontoTuristicoToDelete(PontoTuristico pontoTuristico){
+		pontoTuristicoToDelete = pontoTuristico;
+	}
+	
+	/*Messages Handling*/
+	public void sendMessageToView(String message,Severity severity) {
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, new FacesMessage(severity, message, null));
+	}
+	
+	
+	/*GETTERS AND SETTERS*/
+	public List<PontoTuristico> getPontosTuristicos() {
+		return pontosTuristicos;
+	}
+
+	public void setPontosTuristicos(List<PontoTuristico> pontosTuristicos) {
+		this.pontosTuristicos = pontosTuristicos;
+	}
+
+	public PontoTuristico getPontoTuristico() {
+		return pontoTuristico;
+	}
+
+	public void setPontoTuristico(PontoTuristico pontoTuristico) {
+		this.pontoTuristico = pontoTuristico;
+	}
+
+	public PontoTuristico getPontoTuristicoToEdit() {
+		return pontoTuristicoToEdit;
+	}
+
+	public void setPontoTuristicoToEdit(PontoTuristico pontoTuristicoToEdit) {
+		this.pontoTuristicoToEdit = pontoTuristicoToEdit;
+	}
+
+	public PontoTuristico getPontoTuristicoToDelete() {
+		return pontoTuristicoToDelete;
+	}
+
+	public void setPontoTuristicoToDelete(PontoTuristico pontoTuristicoToDelete) {
+		this.pontoTuristicoToDelete = pontoTuristicoToDelete;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+}
---
+++ agencia/src/main/java/br/edu/univas/agencia/pontos/converter/CidadeConverter.java
@@ -0,0 +1,49 @@
+package br.edu.univas.agencia.pontos.converter;
+
+import java.util.Map;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.edu.univas.agencia.model.Cidade;
+
+@FacesConverter(value = "cidadeConverter")
+public class CidadeConverter implements Converter {
+	
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {		
+		if(value != null && !value.isEmpty()){
+			return this.getAttributesFrom(component).get(value);
+		}else{		
+			return null;
+		}
+	}
+
+	
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+        if (value != null && !"".equals(value)) {  
+        	Cidade entity = (Cidade) value;  
+  
+            this.addAttribute(component, entity);  
+  
+            Integer codigo = entity.getId();  
+            if (codigo != null) {  
+                return String.valueOf(codigo);  
+            }  
+        }  
+  
+        return (String) value; 		
+	}
+	
+    protected void addAttribute(UIComponent component, Cidade o) {  
+        String key = o.getId().toString();  
+        this.getAttributesFrom(component).put(key, o);  
+    }  
+    
+    protected Map<String, Object> getAttributesFrom(UIComponent component) {  
+        return component.getAttributes();  
+    } 
+}
