8175fbe593d94731c9d076bd097dde1f8c2ecc03
Andressa <andressa_faria18@hotmail.com>
2015-12-04 20:34:39 -0200

    Teste unitário Restaurante
    teste unitário Restaurante

--- agencia/src/test/java/br/edu/univas/agencia/agencia/business/RestaurantBusinessTest.java
+++ agencia/src/test/java/br/edu/univas/agencia/agencia/business/RestaurantBusinessTest.java
@@ -11,41 +11,69 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import br.edu.univas.agencia.exception.AgencyException;
+import br.edu.univas.agencia.model.Cidade;
 import br.edu.univas.agencia.model.Pacote;
 import br.edu.univas.agencia.model.PontoTuristico;
 import br.edu.univas.agencia.model.Restaurante;
+import br.edu.univas.agencia.model.RestauranteReserva;
 import br.edu.univas.agencia.restaurante.IRestaurant;
+import br.edu.univas.agencia.restaurante.RestaurantService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RestaurantBusinessTest {
-	
+
 	private static final int PACKAGE_ID = 1;
-	
+
 	// Dependencies
-		@Mock
-		private IRestaurant iRestaurant;
-		
-		@Mock
-		private PackageBusiness packageBusiness;
-		
-		@Mock
-		private Pacote pacote;
-		
-		@Mock
-		private AgencyException agencyException;
-	
-		// Target
-		@InjectMocks
-		private Restaurante restaurantsBusiness;
-		
+	@Mock
+	private RestaurantService restaurantService;
+
+	@Mock
+	private PackageBusiness packageBusiness;
+
+	@Mock
+	private Pacote pacote;
+
+	@Mock
+	private AgencyException agencyException;
+
+	// Target
+	@InjectMocks
+	private RestaurantBussiness restaurantsBusiness;
+
+	@Test
+	public void listRestaurants() throws AgencyException {
+		// given
+		List<Restaurante> restaurants = new ArrayList<Restaurante>();
+		BDDMockito.given(restaurantService.getAvailableRestaurantList(pacote))
+				.willReturn(restaurants);
+
+		// when
+		List<Restaurante> result = (List<Restaurante>) restaurantsBusiness
+				.listRestaurants(pacote);
+
+		// then
+		Assert.assertEquals(restaurants, result);
 	}
-	
+
+	@Test
+	public void createReservation() throws AgencyException{
+		//give
+		RestauranteReserva restaurantReserva = new RestauranteReserva();
+		
+		// when
+		restaurantsBusiness.createReservation(restaurantReserva);
+	}
+}
